309b029ada3e86ef7eaa49c26d95437bc5511443
ToruTAKANO <toru.nb193plus@gmail.com>
2016-03-03 23:58:06 +0900

    changed pub msg type

--- operator_station/src/command_distributor.py
+++ operator_station/src/command_distributor.py
@@ -5,7 +5,7 @@ import time
 
 import rospy
 from yozakura_msgs.msg import ArmState, YozakuraCommand
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Int16MultiArray
 
 # remap-able names
 DEFAULT_NODE_NAME = 'command_distributor'
@@ -15,8 +15,8 @@ DEFAULT_SUB_COMMAND_TOPIC_NAME = 'yozakura_command'
 
 class CommandDistributor(object):
     def __init__(self):
-        self._pub_arm_command = rospy.Publisher(DEFAULT_PUB_ARM_COMMAND_TOPIC_NAME, ArmState, queue_size=10)
-        self._arm_command = ArmState()
+        self._pub_arm_command = rospy.Publisher(DEFAULT_PUB_ARM_COMMAND_TOPIC_NAME, Int16MultiArray, queue_size=10)
+        self._arm_command = Int16MultiArray()
 
         self._pub_motor_command = rospy.Publisher(DEFAULT_PUB_MOTOR_COMMAND_TOPIC_NAME, Float32MultiArray, queue_size=10)
         self._motor_command = Float32MultiArray()
@@ -29,9 +29,8 @@ class CommandDistributor(object):
         self.is_active = True
 
     def command_callback(self, command):
-        self._arm_command = command.arm_vel
+        self._arm_command.data = [-int(command.arm_vel.base_yaw), -int(command.arm_vel.base_pitch), -int(command.arm_vel.base_pitch), int(command.arm_vel.elbow_pitch), int(command.arm_vel.wrist_yaw)]
         self._motor_command.data = [command.wheel_right_vel, command.wheel_left_vel, command.wheel_right_vel, command.wheel_left_vel]
-        rospy.loginfo(self._motor_command)
 
     def publish_data(self):
         if not self.is_active:
